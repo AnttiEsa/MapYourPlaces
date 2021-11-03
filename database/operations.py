a15ff77f5e819cab1191fedc196d73d3320efeb5
Antti Esa <antti.esa@outlook.com>
2021-11-03 20:11:36 +0200

    Added functionality to find column headers in csv

--- database/operations.py
+++ database/operations.py
@@ -1,14 +1,19 @@
-from database import License, Session
-from sqlalchemy import or_
+import csv
 
-def __get__session():
+from sqlalchemy.sql.expression import column
+from database import Places, Session, sessionmaker
+from sqlalchemy import or_
+from sqlalchemy.orm import load_only
+import pandas as pd
+
+def __get_session():
     return Session()
 
-def insert_data(name, address, city, capacity, coordinates_lon, coordinates_lat):
-    new_row = License(name=name, address=address, city=city, capacity=capacity, coordinates_lon = coordinates_lon, 
-    coordinates_lat = coordinates_lat)
+def insert_data(name, x,y):
+    new_row = Places(name=name, x = x, 
+    y = y)
 
-    db = __get__session()
+    db = __get_session()
     """Add a new row to wait for a commit"""
     db.add(new_row)
     """Add a command to database to do the action"""
@@ -17,3 +22,62 @@ def insert_data(name, address, city, capacity, coordinates_lon, coordinates_lat)
     db.close()
     return id
 
+def get():
+    return __get_session().query(Places).all()
+
+def getTable():
+    fields = ['name', 'x', 'y']
+    return __get_session().query(Places).options(load_only(*fields)).all()
+
+def truncateTable():
+    db = __get_session()
+    db.query(Places).delete()
+    db.commit()
+
+def is_float(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+def columnError():
+    with open("data.csv", newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+
+        places = pd.read_csv('data.csv', error_bad_lines=False)
+
+        #Check if the required values are found from the first row
+        if {'Name', 'x', 'y'}.issubset(places.columns):
+            return False
+        else:
+            return True
+
+def getRows():
+    with open("data.csv", newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        line_count = 0
+
+        places = pd.read_csv('data.csv', error_bad_lines=False)
+        name_id = -1
+        x_id    = -1
+        y_id    = -1
+
+        for row in reader:
+
+            if(line_count==0):
+                i=0
+                for columnName in places.columns:
+                    if(columnName == "Name"):
+                        name_id = i
+                    elif(columnName == "x"):
+                        x_id = i
+                    elif(columnName == "y"):
+                        y_id = i
+                    i+=1
+
+            else:
+                if(is_float(row[x_id]) and is_float(row[y_id])):
+                        if(float(row[x_id]) > -90 and float(row[x_id]) < 90 and float(row[y_id]) > -180 and float(row[y_id]) < 180):
+                                insert_data(name=row[name_id], x=row[x_id], y=row[y_id])
+            line_count+=1
